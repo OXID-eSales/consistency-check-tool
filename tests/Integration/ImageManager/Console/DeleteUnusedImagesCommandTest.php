6089b30b10814a349a2d4ceb902ce1d5832e25ef
RahatHameed <therahatrajpoot@gmail.com>
2025-01-24 19:29:09 +0100

    OXDEV-9065 Cover commands by integration tests

---
+++ tests/Integration/ImageManager/Console/DeleteUnusedImagesCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\ConsistencyCheck\ImageManager\Tests\Integration\ImageManager\Console;
+
+use OxidEsales\ConsistencyCheck\ImageManager\Console\DeleteUnusedImagesCommand;
+use OxidEsales\ConsistencyCheck\ImageManager\Entity\ImageEntityInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Factory\ProgressBarFactoryInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\ImageCheckerServiceInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\ImageEntityFilterServiceInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\ImageManagerServiceInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\MessageFormatterServiceInterface;
+use OxidEsales\EshopCommunity\Tests\Integration\IntegrationTestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class DeleteUnusedImagesCommandTest extends IntegrationTestCase
+{
+    #[Test]
+    public function itSuccessfullyDeleteUnusedImages(): void
+    {
+        $sut = $this->getSut();
+
+        $application = new Application();
+        $application->add($sut);
+
+        $commandTester = new CommandTester($application->find('oe:consistency_check:delete-unused-images'));
+        $commandTester->execute([
+            '--type' => uniqid()
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Unused image deletion operation completed.', $output);
+    }
+
+    private function getSut(): DeleteUnusedImagesCommand
+    {
+        return new DeleteUnusedImagesCommand(
+            entities: [$this->createStub(ImageEntityInterface::class)],
+            imageCheckerService: $this->get(ImageCheckerServiceInterface::class),
+            imageManagerService: $this->get(ImageManagerServiceInterface::class),
+            entityFilterService: $this->get(ImageEntityFilterServiceInterface::class),
+            messageFormatter: $this->get(MessageFormatterServiceInterface::class),
+            progressBar: $this->get(ProgressBarFactoryInterface::class),
+            logger: $this->get(LoggerInterface::class)
+        );
+    }
+}
---
+++ tests/Integration/ImageManager/Console/MoveUnusedImagesCommandTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\ConsistencyCheck\ImageManager\Tests\Integration\ImageManager\Console;
+
+use OxidEsales\ConsistencyCheck\ImageManager\Console\MoveUnusedImagesCommand;
+use OxidEsales\ConsistencyCheck\ImageManager\Entity\ImageEntityInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Factory\ProgressBarFactoryInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\ImageCheckerServiceInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\ImageEntityFilterServiceInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\ImageManagerServiceInterface;
+use OxidEsales\ConsistencyCheck\ImageManager\Service\MessageFormatterServiceInterface;
+use OxidEsales\EshopCommunity\Tests\Integration\IntegrationTestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class MoveUnusedImagesCommandTest extends IntegrationTestCase
+{
+    #[Test]
+    public function itSuccessfullyMovesUnusedImages(): void
+    {
+        $sut = $this->getSut();
+
+        $application = new Application();
+        $application->add($sut);
+
+        $commandTester = new CommandTester($application->find('oe:consistency_check:move-unused-images'));
+
+        $commandTester->execute([
+            '--type' => uniqid(),
+            '--destination' => uniqid(),
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Unused image move operation completed.', $output);
+    }
+
+
+    private function getSut(): MoveUnusedImagesCommand
+    {
+        return new MoveUnusedImagesCommand(
+            entities: [$this->createStub(ImageEntityInterface::class)],
+            imageCheckerService: $this->get(ImageCheckerServiceInterface::class),
+            imageManagerService: $this->get(ImageManagerServiceInterface::class),
+            entityFilterService: $this->get(ImageEntityFilterServiceInterface::class),
+            messageFormatter: $this->get(MessageFormatterServiceInterface::class),
+            progressBar: $this->get(ProgressBarFactoryInterface::class),
+            logger: $this->get(LoggerInterface::class)
+        );
+    }
+}
