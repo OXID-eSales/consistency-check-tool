5b5f47390a65820cefb2efaa482a06aca9555377
RahatHameed <therahatrajpoot@gmail.com>
2025-11-24 13:32:41 +0100

    OXDEV-9944 Create Shared Exception Classes

---
+++ src/Shared/Exception/InvalidFileFormatException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\ConsistencyCheck\Shared\Exception;
+
+use Exception;
+
+final class InvalidFileFormatException extends Exception
+{
+}
