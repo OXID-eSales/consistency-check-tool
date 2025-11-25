11e6f9fa899293af2e7cdc45e7586281d8ad002b
RahatHameed <therahatrajpoot@gmail.com>
2025-11-25 20:19:30 +0100

    OXDEV-9944 Add SeoUrlDto for SEO URL data transfer

---
+++ tests/Unit/SeoUrl/DTO/SeoUrlDtoTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\ConsistencyCheck\Tests\Unit\SeoUrl\DTO;
+
+use OxidEsales\ConsistencyCheck\SeoUrl\DTO\SeoUrlDto;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class SeoUrlDtoTest extends TestCase
+{
+    #[Test]
+    public function itReturnsCorrectGetterValues(): void
+    {
+        $sut = new SeoUrlDto(
+            objectId: $expectedObjectId = uniqid(),
+            ident: $expectedIdent = uniqid(),
+            shopId: $expectedShopId = rand(1, 10),
+            languageId: $expectedLanguageId = rand(0, 5),
+            stdUrl: $expectedStdUrl = uniqid(),
+            seoUrl: $expectedSeoUrl = uniqid() . '.html',
+            type: $expectedType = 'oxarticle',
+            fixed: $expectedFixed = rand(0, 1),
+            expired: $expectedExpired = rand(0, 1),
+            params: $expectedParams = uniqid(),
+            timestamp: $expectedTimestamp = date('Y-m-d H:i:s'),
+        );
+
+        $this->assertSame($expectedObjectId, $sut->getObjectId());
+        $this->assertSame($expectedIdent, $sut->getIdent());
+        $this->assertSame($expectedShopId, $sut->getShopId());
+        $this->assertSame($expectedLanguageId, $sut->getLanguageId());
+        $this->assertSame($expectedStdUrl, $sut->getStdUrl());
+        $this->assertSame($expectedSeoUrl, $sut->getSeoUrl());
+        $this->assertSame($expectedType, $sut->getType());
+        $this->assertSame($expectedFixed, $sut->getFixed());
+        $this->assertSame($expectedExpired, $sut->getExpired());
+        $this->assertSame($expectedParams, $sut->getParams());
+        $this->assertSame($expectedTimestamp, $sut->getTimestamp());
+    }
+}
