3fbce98306e945c0bad79ab0a50b536ee3750157
Rahat hameed <therahatrajpoot@gmail.com>
2026-04-17 10:11:48 +0200

    OXDEV-10208 Update UserCredentialDtoFactoryInterface

--- src/ExportByFilter/Filter/Credentials/Factory/UserCredentialDtoFactoryInterface.php
+++ src/ExportByFilter/Filter/Credentials/Factory/UserCredentialDtoFactoryInterface.php
@@ -9,12 +9,23 @@ declare(strict_types=1);
 
 namespace OxidEsales\ConsistencyCheck\ExportByFilter\Filter\Credentials\Factory;
 
+use OxidEsales\ConsistencyCheck\Export\Factory\DtoFactoryInterface;
 use OxidEsales\ConsistencyCheck\ExportByFilter\Filter\Credentials\Dto\UserCredentialDtoInterface;
 
-interface UserCredentialDtoFactoryInterface
+/**
+ * @phpstan-type UserTableRow array{
+ *     OXID: string,
+ *     OXACTIVE: string,
+ *     OXCREATE: string,
+ *     OXTIMESTAMP: string,
+ *     OXLASTORDER: string,
+ *     OXPASSWORD: string
+ * }
+ */
+interface UserCredentialDtoFactoryInterface extends DtoFactoryInterface
 {
     /**
-     * @param array<string, mixed> $data
+     * @param UserTableRow $data
      */
-    public function create(array $data): UserCredentialDtoInterface;
+    public function createFromArray(array $data): UserCredentialDtoInterface;
 }
